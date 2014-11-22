bbb5b2790f3a965e604a4cb32c60cac3db5b39e0
Bing Liu <v.bingliu@gmail.com>
2014-11-22 15:25:25 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+web_pdf_downloader
+==================
+
+Download all PDF files in single webpage  
+Input: Web page URL, PDF Download directory
---
+++ pdf_downloader.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 22 14:57:12 2014
+
+@author: Bing Liu
+"""
+
+import lxml.html, urllib2, urlparse, os
+
+def main(base_url, save_dir):    
+    # fetch the page
+    res = urllib2.urlopen(base_url)
+    
+    # parse the response into an xml tree
+    tree = lxml.html.fromstring(res.read())
+    
+    # construct a namespace dictionary to pass to the xpath() call
+    # this lets us use regular expressions in the xpath
+    ns = {'re': 'http://exslt.org/regular-expressions'}
+    
+    # iterate over all <a> tags whose href ends in ".pdf" (case-insensitive)
+    for node in tree.xpath('//a[re:test(@href, "\.pdf$", "i")]', namespaces=ns): 
+        # print the href, joining it to the base_url
+        file_url =  urlparse.urljoin(base_url, node.attrib['href'])
+        print 'Downloading %s...' % file_url.split('/')[-1]
+        download_file = urllib2.urlopen(file_url)
+        data = download_file.read()
+        with open(save_dir + '/' + file_url.split('/')[-1], "wb") as f:
+            f.write(data)
+    print 'All DONE!'
+
+if __name__ == '__main__':
+    url = 'http://cs229.stanford.edu/materials.html'
+    save_dir = 'cs229'
+    if os.path.exists(save_dir) is False:
+        os.mkdir(save_dir)
+    main(url, save_dir)
